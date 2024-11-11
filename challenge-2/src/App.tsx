94394b1bba7f0c59ad8e530973e7edf8be69f06a
= <=>
2024-11-11 10:53:39 +0700

    update

--- challenge-2/src/App.tsx
+++ challenge-2/src/App.tsx
@@ -1,42 +1,10 @@
-// import dedotLogo from './assets/dedot-dark-logo.png';
-// import { Container, Flex, Heading } from '@chakra-ui/react';
-
-// function App() {
-//   // 1. Connect to SubWallet
-//   // 2. Show connected account (name & address)
-//   // 3. Initialize `DedotClient` to connect to the network (Westend testnet)
-//   // 4. Fetch & show balance for connected account
-//   // 5. Build a form to transfer balance (destination address & amount to transfer)
-//   // 6. Check transaction status (in-block & finalized)
-//   // 7. Check transaction result (success or not)
-//   // 8. Subscribe to balance changing
-
-//   return (
-//     <Container maxW='container.md' my={16}>
-//       <Flex justifyContent='center'>
-//         <a href='https://dedot.dev' target='_blank'>
-//           <img width='100' src={dedotLogo} className='logo' alt='Vite logo' />
-//         </a>
-//       </Flex>
-//       <Heading my={4} textAlign='center'>
-//         Open Hack Dedot
-//       </Heading>
-//     </Container>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import { Injected, InjectedAccount, InjectedWindowProvider, InjectedWindow } from '@polkadot/extension-inject/types';
 import { DedotClient, WsProvider } from 'dedot';
 import { WestendApi } from '@dedot/chaintypes';
-import { WESTEND } from './networks';
+import { WESTEND_PEOPLE } from './networks';
 import { FrameSystemAccountInfo } from '@dedot/chaintypes/westend';
 import { formatBalance } from './utils';
-
-// Thay thế bằng endpoint của Westend
-const WESTEND_ENDPOINT = 'wss://westend-rpc.polkadot.io';
 
 const App: React.FC = () => {
   const [accounts, setAccounts] = useState<InjectedAccount[]>([]);
@@ -50,6 +18,11 @@ const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [destAddress, setDestAddress] = useState<string>('');
   const [amount, setAmount] = useState<number>(1);
+
+  const [displayName, setDisplayName] = useState<any>('');
+  const [email, setEmail] = useState<any>('');
+  const [discord, setDiscord] = useState<any>('');
+
   //   // 1. Connect to SubWallet
   //   // 2. Show connected account (name & address)
 
@@ -70,7 +43,7 @@ const App: React.FC = () => {
   //   // 3. Initialize `DedotClient` to connect to the network (Westend testnet)
   const initializeClient = async (endpoint: string): Promise<DedotClient<WestendApi>> => {
     // initialize the client and connect to the network
-    const client = new DedotClient<WestendApi>(new WsProvider(WESTEND.endpoint));
+    const client = new DedotClient<WestendApi>(new WsProvider(WESTEND_PEOPLE.endpoint));
     await client.connect();
 
     return client;
@@ -83,7 +56,7 @@ const App: React.FC = () => {
     const balance: FrameSystemAccountInfo = await client.query.system.account(account.address);
 
     // Get free/transferable balance
-    const freeBalance = formatBalance(balance.data.free, WESTEND.decimals);
+    const freeBalance = formatBalance(balance.data.free, WESTEND_PEOPLE.decimals);
     return freeBalance;
   };
 
@@ -98,9 +71,8 @@ const App: React.FC = () => {
     setError(null);
 
     try {
-    
       // Convert the amount (DOT, or WND) to Planck unit
-      const amountToTransfer: bigint = BigInt(amount) * BigInt(Math.pow(10, WESTEND.decimals));
+      const amountToTransfer: bigint = BigInt(amount) * BigInt(Math.pow(10, WESTEND_PEOPLE.decimals));
 
       await client.tx.balances
         .transferKeepAlive(destAddress, amountToTransfer)
@@ -127,19 +99,20 @@ const App: React.FC = () => {
   };
 
   //   // 6. Check transaction status (in-block & finalized)
-//   // 7. Check transaction result (success or not)
-//   // 8. Subscribe to balance changing
+  //   // 7. Check transaction result (success or not)
+  //   // 8. Subscribe to balance changing
 
   const subscribeToBalanceChanges = async (
-    client: DedotClient<any>, connectedAccounts: InjectedAccount
-  ) : Promise<() => void>  => {
+    client: DedotClient<any>,
+    connectedAccounts: InjectedAccount,
+  ): Promise<() => void> => {
     try {
       const account: InjectedAccount = connectedAccounts; // get from accounts list - 6.2
 
       // Pass in a callback to be called whenver the balance is changed/updated
       const unsub = await client.query.system.account(account.address, (balance: FrameSystemAccountInfo) => {
         // Get free/transferable balance
-        const freeBalance = formatBalance(balance.data.free, WESTEND.decimals);  
+        const freeBalance = formatBalance(balance.data.free, WESTEND_PEOPLE.decimals);
         setBalance(freeBalance);
       });
       return unsub;
@@ -148,6 +121,66 @@ const App: React.FC = () => {
       throw new Error('Failed to subscribe to balance changes');
     }
   };
+
+  // const handleIdentity = async () => {
+  //   if (!client || !accounts || !injected) {
+  //     setError('Missing necessary information (client, account, or injected wallet)');
+  //     return;
+  //   }
+
+  //   setIsLoading(true);
+  //   setError(null);
+
+  //   if (!displayName) {
+  //     alert('Enter displayName!');
+  //     return;
+  //   }
+
+  //   if (!email) {
+  //     alert('Enter email!');
+  //     return;
+  //   }
+
+  //   if (!discord) {
+  //     alert('Enter Discord!');
+  //     return;
+  //   }
+
+  //   try {
+  //     await client.tx.identity
+  //       .setIdentity({
+  //         display: displayName,
+  //         email: email,
+  //         additional:[],
+  //         image: '0x123abc456def',
+  //         url: "",
+  //         twitter: "",
+  //         github: "",
+  //         linkedin: "",
+  //       }
+  //       )
+  //       .signAndSend(accounts[0].address, { signer: injected.signer }, (result) => {
+  //         console.log(result.status);
+
+  //         // 'BestChainBlockIncluded': Transaction is included in the best block of the chain
+  //         // 'Finalized': Transaction is finalized
+  //         if (result.status.type === 'BestChainBlockIncluded' || result.status.type === 'Finalized') {
+  //           if (result.dispatchError) {
+  //             const error = `${JSON.stringify(Object.values(result.dispatchError))}`;
+  //             setError(`Identity error: ${error}`);
+  //           } else {
+  //             alert('Identity successful!');
+  //           }
+  //         }
+  //       });
+  //   } catch (err) {
+  //     console.error('Identity failed:', err);
+  //     setError('Identity failed. Please try again.');
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
   useEffect(() => {
     const setup = async () => {
       try {
@@ -155,13 +188,13 @@ const App: React.FC = () => {
         setAccounts(connectedAccounts);
 
         if (connectedAccounts.length > 0) {
-          const dedotClient = await initializeClient(WESTEND_ENDPOINT);
+          const dedotClient = await initializeClient(WESTEND_PEOPLE.endpoint);
           setClient(dedotClient);
 
           const balance = await fetchBalance(dedotClient, connectedAccounts[0]);
           setBalance(balance);
 
-          if(dedotClient) {
+          if (dedotClient) {
             const unsub = await subscribeToBalanceChanges(dedotClient, connectedAccounts[0]);
 
             // Cleanup khi component unmount
@@ -169,10 +202,7 @@ const App: React.FC = () => {
               unsub(); // Hủy đăng ký khi component bị hủy
             };
           }
-          
         }
-
-        
       } catch (err) {
         console.error('Error:', err);
         setError('Failed to connect or fetch data');
@@ -189,49 +219,47 @@ const App: React.FC = () => {
       ) : (
         <div>
           {accounts.length > 0 ? (
-            accounts.map((account, index) => (
-              <p key={index}>
-                {account.name || 'Unnamed'} ({account.address})
-              </p>
-            ))
+            <p>
+              {accounts[0].name || 'Unnamed'} ({accounts[0].address})
+            </p>
           ) : (
             <p>Loading accounts...</p>
           )}
 
           {balance ? <p>Free Balance: {balance} WND</p> : <p>Loading balance...</p>}
 
-          <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: '100%',
-            padding: '20px',
-            backgroundColor: '#f5f5f5',
-            borderRadius: '5px',
-            marginBottom: '10px',
-            boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
-            gap:'2px'
-          }}>
-            <h1 >Transfer Funds</h1>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              width: '100%',
+              padding: '20px',
+              backgroundColor: '#f5f5f5',
+              borderRadius: '5px',
+              marginBottom: '10px',
+              boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+              gap: '2px',
+            }}>
+            <h1>Transfer Funds</h1>
             {error && <p style={{ color: 'red' }}>{error}</p>}
-            <div >
+            <div>
               <label
-              style={{
-                display: 'flex',
-              flexDirection:'column'
-              }}
-              >
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
                 Destination Address:
                 <input
-                style={{
-                  border: '1px solid',
-                  padding: '4px',
-                  marginBottom: '10px',
-                  width: '300px',
-                  borderRadius: '5px',
-                  outline: 'none',
-                }}
+                  style={{
+                    border: '1px solid',
+                    padding: '4px',
+                    marginBottom: '10px',
+                    width: '300px',
+                    borderRadius: '5px',
+                    outline: 'none',
+                  }}
                   type='text'
                   value={destAddress}
                   onChange={(e) => setDestAddress(e.target.value)}
@@ -241,42 +269,140 @@ const App: React.FC = () => {
             </div>
             <div>
               <label
-              style={{
-                display: 'flex',
-              flexDirection:'column'
-              }}
-              >
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
                 Amount:
                 <input
-                 style={{
-                  border: '1px solid',
-                  padding: '4px',
-                  marginBottom: '10px',
-                  width: '300px',
-                  borderRadius: '5px',
-                  outline: 'none',
-                }}
-                
+                  style={{
+                    border: '1px solid',
+                    padding: '4px',
+                    marginBottom: '10px',
+                    width: '300px',
+                    borderRadius: '5px',
+                    outline: 'none',
+                  }}
                   value={amount}
                   onChange={(e) => setAmount(Number(e.target.value))}
                   placeholder='Enter amount to transfer'
                 />
               </label>
             </div>
-            <button 
-            style={{
-              backgroundColor: 'green',
-              color: 'white',
-              padding: '10px 20px',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              outline: 'none',
-              border: 'none',
-            }
-            
-            }
-            onClick={handleTransfer} disabled={isLoading}>
+            <button
+              style={{
+                backgroundColor: 'green',
+                color: 'white',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                outline: 'none',
+                border: 'none',
+              }}
+              onClick={handleTransfer}
+              disabled={isLoading}>
               {isLoading ? 'Transferring...' : 'Transfer'}
+            </button>
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              width: '100%',
+              padding: '20px',
+              backgroundColor: '#f5f5f5',
+              borderRadius: '5px',
+              marginBottom: '10px',
+              boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+              gap: '2px',
+            }}>
+            <h1>Identity Information</h1>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <div>
+              <label
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                Display Name:
+                <input
+                  style={{
+                    border: '1px solid',
+                    padding: '4px',
+                    marginBottom: '10px',
+                    width: '300px',
+                    borderRadius: '5px',
+                    outline: 'none',
+                  }}
+                  type='text'
+                  value={displayName}
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div>
+              <label
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                Email:
+                <input
+                  style={{
+                    border: '1px solid',
+                    padding: '4px',
+                    marginBottom: '10px',
+                    width: '300px',
+                    borderRadius: '5px',
+                    outline: 'none',
+                  }}
+                  type='text'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div>
+              <label
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                Discord:
+                <input
+                  style={{
+                    border: '1px solid',
+                    padding: '4px',
+                    marginBottom: '10px',
+                    width: '300px',
+                    borderRadius: '5px',
+                    outline: 'none',
+                  }}
+                  type='text'
+                  value={discord}
+                  onChange={(e) => setDiscord(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <button
+            // onClick={handleIdentity}
+              style={{
+                backgroundColor: 'green',
+                color: 'white',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                outline: 'none',
+                border: 'none',
+              }}
+              disabled={isLoading}>
+              {isLoading ? 'Identity...' : 'Identity'}
             </button>
           </div>
         </div>
